893dae9eeb9576e79a38d3e8c09ed71eafa7710d
Ghazanfar-ijaz <78793183+Ghazanfar-ijaz@users.noreply.github.com>
2021-02-11 23:20:35 +0100

    Create pyramid2.js

---
+++ pyramid2.js
@@ -0,0 +1,10 @@
+let pyr = '*'
+
+console.log(pyr + '              ' + pyr)
+console.log(pyr + pyr + '            ' + pyr + pyr)
+console.log(pyr + pyr + pyr + '          ' + pyr + pyr + pyr)
+console.log(pyr + pyr + pyr + pyr + '        ' + pyr + pyr + pyr + pyr)
+console.log(pyr + pyr + pyr + pyr + pyr + '      ' + pyr + pyr + pyr + pyr + pyr)
+console.log(pyr + pyr + pyr + pyr + pyr + pyr + '    ' + pyr + pyr + pyr + pyr + pyr + pyr)
+console.log(pyr + pyr + pyr + pyr + pyr + pyr + pyr + '  ' + pyr + pyr + pyr + pyr + pyr + pyr + pyr)
+console.log(pyr + pyr + pyr + pyr + pyr + pyr + pyr + pyr + pyr + pyr + pyr + pyr + pyr + pyr + pyr + pyr)
