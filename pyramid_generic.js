a2691e910b691c6f3ee1776b8ca66ea704457683
Ghazanfar-ijaz <78793183+Ghazanfar-ijaz@users.noreply.github.com>
2021-02-11 23:25:41 +0100

    Create pyramid_generic.js

---
+++ pyramid_generic.js
@@ -0,0 +1,10 @@
+let pyr = 'A'
+
+console.log(pyr + '              ' + pyr)
+console.log(pyr + pyr + '            ' + pyr + pyr)
+console.log(pyr + pyr + pyr + '          ' + pyr + pyr + pyr)
+console.log(pyr + pyr + pyr + pyr + '        ' + pyr + pyr + pyr + pyr)
+console.log(pyr + pyr + pyr + pyr + pyr + '      ' + pyr + pyr + pyr + pyr + pyr)
+console.log(pyr + pyr + pyr + pyr + pyr + pyr + '    ' + pyr + pyr + pyr + pyr + pyr + pyr)
+console.log(pyr + pyr + pyr + pyr + pyr + pyr + pyr + '  ' + pyr + pyr + pyr + pyr + pyr + pyr + pyr)
+console.log(pyr + pyr + pyr + pyr + pyr + pyr + pyr + pyr + pyr + pyr + pyr + pyr + pyr + pyr + pyr + pyr)
